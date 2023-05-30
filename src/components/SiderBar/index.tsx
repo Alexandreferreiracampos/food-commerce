1359b182b98bf62e67e7a41a559a9f5516572aac
Alexandreferreiracampos <alexandrerazrmaxx@gmail.com>
2023-05-30 15:57:49 -0300

    criando estrutura do projeto

---
+++ src/components/SiderBar/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+export function Siderbar(){
+    return (
+        <Container>Sidebar</Container>
+    )
+}
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Siderbar } from '../../components/SiderBar'
+import { Container} from './styles'
+
+export default function Main() {
+  return (
+    <Container>
+      <Siderbar/>
+      <h1>Main</h1>
+    </Container>
+  )
+}
